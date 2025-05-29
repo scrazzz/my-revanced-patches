ab62f89d6641ced98f3c91215df106f076308d87
scrazzz <scruzism@mail.com>
2025-05-29 15:58:19 +0530

    chore: cleanup files and fix import errors

--- patches/src/main/kotlin/app/revanced/util/BytecodeUtils.kt
+++ patches/src/main/kotlin/app/revanced/util/BytecodeUtils.kt
@@ -11,9 +11,6 @@ import app.revanced.patcher.patch.BytecodePatchContext
 import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.util.proxy.mutableTypes.MutableClass
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
-import app.revanced.patches.shared.misc.mapping.get
-import app.revanced.patches.shared.misc.mapping.resourceMappingPatch
-import app.revanced.patches.shared.misc.mapping.resourceMappings
 import app.revanced.util.InstructionUtils.Companion.branchOpcodes
 import app.revanced.util.InstructionUtils.Companion.returnOpcodes
 import app.revanced.util.InstructionUtils.Companion.writeOpcodes
@@ -236,38 +233,6 @@ fun MutableMethod.addInstructionsAtControlFlowLabel(
 
     // Original instruction is now after the inserted patch instructions,
     // and the original control flow label is on the first instruction of the patch code.
-}
-
-/**
- * Get the index of the first instruction with the id of the given resource id name.
- *
- * Requires [resourceMappingPatch] as a dependency.
- *
- * @param resourceName the name of the resource to find the id for.
- * @return the index of the first instruction with the id of the given resource name, or -1 if not found.
- * @throws PatchException if the resource cannot be found.
- * @see [indexOfFirstResourceIdOrThrow], [indexOfFirstLiteralInstructionReversed]
- */
-fun Method.indexOfFirstResourceId(resourceName: String): Int {
-    val resourceId = resourceMappings["id", resourceName]
-    return indexOfFirstLiteralInstruction(resourceId)
-}
-
-/**
- * Get the index of the first instruction with the id of the given resource name or throw a [PatchException].
- *
- * Requires [resourceMappingPatch] as a dependency.
- *
- * @throws [PatchException] if the resource is not found, or the method does not contain the resource id literal value.
- * @see [indexOfFirstResourceId], [indexOfFirstLiteralInstructionReversedOrThrow]
- */
-fun Method.indexOfFirstResourceIdOrThrow(resourceName: String): Int {
-    val index = indexOfFirstResourceId(resourceName)
-    if (index < 0) {
-        throw PatchException("Found resource id for: '$resourceName' but method does not contain the id: $this")
-    }
-
-    return index
 }
 
 /**
