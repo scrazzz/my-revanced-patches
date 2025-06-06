55345845581f864994e135098fa7e266fa72d51c
scrazzz <scruzism@mail.com>
2025-06-06 10:44:55 +0530

    fix: update patch names

--- patches/src/main/kotlin/app/revanced/patches/scrl/UnlockPremiumPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/scrl/UnlockPremiumPatch.kt
@@ -10,8 +10,8 @@ val unlockPremiumPatch = bytecodePatch(
     compatibleWith("com.appostrophe.scrl")
 
     execute {
-        // There will be a pop-up shown to upgrade when the app opens
-        // for the first time, simply ignore that by pressing (X) on top right corner
+        // There will be a pop-up shown to upgrade when the app opens for the first time,
+        // simply ignore that by pressing (X) on top right corner
         getPremiumStatusFingerprint.method.returnEarly(true)
     }
 }
